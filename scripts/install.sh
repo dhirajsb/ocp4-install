db2507e7bd4e91a1a02e1193b47c05f8b6ea7577
Dhiraj Bokde <dbokde@redhat.com>
2019-02-21 21:55:51 -0800

    Initial version

---
+++ scripts/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# update apk
+#sudo apk update
+
+# install pre-requisites
+#sudo apk --no-cache add git
+#sudo apk --no-cache add qemu-system-x86_64 libvirt
+
+# get OCP4 installer source
+#git clone https://github.com/openshift/installer.git
+#cd installer
+#git fetch --tags
+#git checkout v0.12.0
+
+# build 
+
+# run installer and create OCP4 cluster
+
+#./openshift-installer 
